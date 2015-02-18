6561f95bba891a65c7fdd892759a6cf84ba2b879
Mikael Hallendal <hallski@hallski.org>
2015-02-19 00:12:57 +0100

    Remove boilerplate from tests

--- TransformationKitTests/TransformationKitTests.m
+++ TransformationKitTests/TransformationKitTests.m
@@ -16,16 +16,6 @@
 
 @implementation TransformationKitTests
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
 - (void)testMapping
 {
     NSArray *array = @[@1, @2, @3, @4];

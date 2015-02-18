2decd05e046ceef9fb19776834c8c561536fe198
Mikael Hallendal <hallski@hallski.org>
2015-02-18 20:48:24 +0100

    Initial Commit

---
+++ TransformationKit/TransformationKit.h
@@ -0,0 +1,13 @@
+//
+//  TransformationKit.h
+//  TransformationKit
+//
+//  Created by Mikael Hallendal on 18/02/15.
+//  Copyright (c) 2015 Mikael Hallendal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TransformationKit : NSObject
+
+@end
---
+++ TransformationKit/TransformationKit.m
@@ -0,0 +1,13 @@
+//
+//  TransformationKit.m
+//  TransformationKit
+//
+//  Created by Mikael Hallendal on 18/02/15.
+//  Copyright (c) 2015 Mikael Hallendal. All rights reserved.
+//
+
+#import "TransformationKit.h"
+
+@implementation TransformationKit
+
+@end

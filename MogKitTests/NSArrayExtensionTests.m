f7eefa4a6bcc20593392458e36ea607374c8bac8
Mikael Hallendal <hallski@hallski.org>
2015-02-26 22:31:58 +0100

    Remove unused test setup/teardown

--- MogKitTests/NSArrayExtensionTests.m
+++ MogKitTests/NSArrayExtensionTests.m
@@ -7,20 +7,6 @@
 
 @implementation NSArrayExtensionTests
 
-- (void)setUp
-{
-    [super setUp];
-
-    // Setup code here.
-}
-
-- (void)tearDown
-{
-    // Tear-down code here.
-
-    [super tearDown];
-}
-
 - (void)testArrayTransduce
 {
     NSArray *array = @[@1, @2, @3, @4, @5];
